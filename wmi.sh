9e373248381b8bb4616572b79f6fe191ef98950c
Victor T <tavic@mia.debian>
2024-05-23 03:35:41 -0400

    update

--- wmi.sh
+++ wmi.sh
@@ -17,6 +17,7 @@ sudo apt-get	update				-y
 #sudo apt-get	install       firefox           -y
 #sudo apt-get	install       xinit             -y
 #sudo apt	install       xrandr            -y
+#sudo apt        install       xcompmgr          -y
 sudo apt        install       build-essential   -y
 sudo apt	install       libx11-dev        -y
 sudo apt	install       libxft-dev        -y
@@ -24,11 +25,12 @@ sudo apt	install       gcc               -y
 sudo apt-get	install       libxinerama-dev   -y
 #sudo apt	install       pulseaudio        -y
 #sudo apt	install       nitrogen          -y
+#sudo apt       install       xdotool		-y
 
 cd /home/$user/build
-git clone https://git.suckless.org/dwm
-git clone https://git.suckless.org/dmenu
-git clone https://git.suckless.org/st
+git clone https://github.com/mr-v1c/dwm.git
+git clone https://github.com/mr-v1c/mr-dmenu.git
+git clone https://github.com/mr-v1c/mr-st.git
 
 cd dwm
 sudo make clean install
@@ -45,6 +47,9 @@ sudo make clean install
 # Uncomment if you chose to install xrandr
 #echo 'xranrd --output VGA-1 --mode 1920x1080 --rate 60 &' >> /home/$user/.xsessionrc
 
+# Uncomment if you chose to xcompmgr
+#echo 'xcompmgr &' >> /home/$user/.xsessionrc
+
 # Uncomment if you chose to install pulseaudio
 #echo'/usr/bin/pulseaudio &' >> /home/$user/.xsessionrc
 
@@ -52,4 +57,10 @@ sudo make clean install
 #echo "nitrogen --set-auto --random /home/$user/Pictures/wallpapers/ &" >> /home/$user/.xsessionrc
 
 echo $'while true;do\nxsetroot -name "$(/bin/date +"| %l:%M %p || %A %b %d || %Y |")"\nsleep 1m\ndone &' >> /home/$user/.xsessionrc
+
 echo 'exec dwm' >> /home/$user/.xsessionrc
+
+#uncomment if you installed xdotool
+#echo 'xdotool key super+F5' >>/home/$user/.xsesionrc
+
+
